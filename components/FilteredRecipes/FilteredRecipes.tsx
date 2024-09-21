c284b7f98063943d6a515cf9ba455f06b52ed78f
Vincent Hagen <vinnie@script4web.nl>
2024-09-21 19:53:44 +0200

    Add suspend to search for the useSearchParam thing

--- components/FilteredRecipes/FilteredRecipes.tsx
+++ components/FilteredRecipes/FilteredRecipes.tsx
@@ -3,9 +3,18 @@
 import useSearch from '@/hooks/useSearch';
 import RecipeList from '../RecipeList';
 import { Recipe } from '@/api/recipes';
+import { Suspense } from 'react';
 
-export default function FilteredRecipes({ recipes }: { recipes: Recipe[] }) {
+function FilteredRecipesUsingSearch({ recipes }: { recipes: Recipe[] }) {
   const filteredRecipes = useSearch(recipes);
 
   return <RecipeList recipes={filteredRecipes} />;
+}
+
+export default function FilteredRecipes({ recipes }: { recipes: Recipe[] }) {
+  return (
+    <Suspense fallback={<span>Loading...</span>}>
+      <FilteredRecipesUsingSearch recipes={recipes} />
+    </Suspense>
+  );
 }
