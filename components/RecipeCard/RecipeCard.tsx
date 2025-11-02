7ab02462136f593ccc5dde2f89983263cea5e68a
Vincent Hagen <vinnie@script4web.nl>
2025-11-02 12:23:34 +0100

    Fix selection button

--- components/RecipeCard/RecipeCard.tsx
+++ components/RecipeCard/RecipeCard.tsx
@@ -1,14 +1,10 @@
 import { Recipe } from '../../api/recipes';
 import RecipeMeta from '../RecipeMeta';
 import RecipeImage from '../RecipeImage';
-import { ReactNode, use } from 'react';
-import { SelectionContext } from '../../context/selectionContext';
+import { ReactNode } from 'react';
+import SelectButton from './SelectButton';
 
 export default function RecipeCard({ recipe }: { recipe: Recipe }): ReactNode {
-  const { has, add, remove, isLoaded } = use(SelectionContext);
-
-  const isSelected = has(recipe);
-
   return (
     <li className="relative col-span-1 flex divide-y divide-gray-200 rounded-lg bg-white text-center shadow border border-gray-100 hover:border-fuchsia-100">
       <div className="flex flex-1">
@@ -22,32 +18,7 @@ export default function RecipeCard({ recipe }: { recipe: Recipe }): ReactNode {
           </h3>
           <RecipeMeta tags={recipe.tags} type={recipe.type} />
         </a>
-        {isLoaded && (
-          <button
-            role="checkbox"
-            aria-selected={isSelected}
-            type="button"
-            className={[
-              'absolute right-4 top-4 w-10 h-10 font-bold text-xl text-center rounded-full',
-              'bg-fuchsia-100 text-gray-400',
-              'hover:text-fuchsia-50 focus:text-fuchsia-50',
-              'hover:bg-fuchsia-700 focus:bg-fuchsia-700',
-              'focus:outline-none focus:ring-2 focus:ring-fuchsia-700 focus:ring-offset-2',
-              isSelected ? 'ring-2 ring-fuchsia-200 ring-offset-2' : '',
-            ]
-              .filter((a) => !!a)
-              .join(' ')}
-            onClick={() => {
-              if (has(recipe)) {
-                remove(recipe);
-              } else {
-                add(recipe);
-              }
-            }}
-          >
-            {has(recipe) ? '-' : '+'}
-          </button>
-        )}
+        <SelectButton recipe={recipe} />
       </div>
     </li>
   );
---
+++ components/RecipeCard/SelectButton.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { ReactNode, use } from 'react';
+import { SelectionContext } from '../../context/selectionContext';
+import { Recipe } from '../../api/recipes';
+
+type Props = {
+  recipe: Recipe;
+};
+
+export default function SelectButton({ recipe }: Props): ReactNode {
+  const { has, add, remove, isLoaded } = use(SelectionContext);
+  const isSelected = has(recipe);
+
+  if (!isLoaded) {
+    return null;
+  }
+
+  return (
+    <button
+      role="checkbox"
+      aria-selected={isSelected}
+      aria-label={
+        isSelected
+          ? 'Verwijder recept uit selectie'
+          : 'Voeg recept toe aan selectie'
+      }
+      type="button"
+      className={[
+        'absolute right-4 top-4 w-10 h-10 font-bold text-xl text-center rounded-full',
+        'bg-fuchsia-100 text-gray-400',
+        'hover:text-fuchsia-50 focus:text-fuchsia-50',
+        'hover:bg-fuchsia-700 focus:bg-fuchsia-700',
+        'focus:outline-none focus:ring-2 focus:ring-fuchsia-700 focus:ring-offset-2',
+        isSelected ? 'ring-2 ring-fuchsia-200 ring-offset-2' : '',
+      ]
+        .filter(Boolean)
+        .join(' ')}
+      onClick={() => {
+        if (isSelected) {
+          remove(recipe);
+        } else {
+          add(recipe);
+        }
+      }}
+    >
+      {isSelected ? '-' : '+'}
+    </button>
+  );
+}
